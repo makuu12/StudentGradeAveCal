12bbfc9d67775a1cffa5ddf026fe369742b0edf6
Makoo <141389899+makuu12@users.noreply.github.com>
2023-09-25 16:11:43 +0800

    Added Whole File of Student Grades Average Calculator
    Added Whole File of Student Grades Average Calculator

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        [HttpGet]
+
+        public ActionResult Index(Models.Grade m)
+        {
+            m.total = m.english + m.math + m.science;
+            m.average = m.total / 3;
+
+            if (m.average >= 75)
+            {
+                m.grade = "PASSED";
+            }
+            else
+            {
+                m.grade = "FAILED";
+            }
+
+            return View(m);
+
+
+        }
+
+
+    }
+}
---
+++ WebApplication1/Models/Grade.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+
+{
+    public class Grade
+    {
+        public int english { get; set; }
+
+        public int math { get; set; }
+
+        public int science { get; set; }
+
+        public int total { get; set; }
+
+        public int average { get; set; }
+
+        public String grade { get; set; }
+    }
+}
